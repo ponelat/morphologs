db2b7d1366af947c325628300a1390f71db1861b
josh <josh@ponelat.com>
2014-11-12 12:05:19 +0200

    Initial. Run with browser-sync :)

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      bower:{
+        options: {
+          banner: '',
+          stripBanners: false
+        },
+        files: {
+          'vendor/pure.min.css': ['./bower_components/pure/pure-min.css']
+        } 
+      }
+    },
+
+   'bower-install-simple': {
+     all:{}
+   }
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+
+      gruntfile: {
+        src: 'Gruntfile.js'
+      }
+    },
+
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-bower-install-simple');
+
+  // Default task.
+  grunt.registerTask('bower', ['bower-install-simple','concat:bower']);
+  grunt.registerTask('default', ['jshint', 'concat']);
+
+};
